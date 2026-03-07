a48a091c27792b84c64ee2bf5c98a6bca2ef4e15
knnthcrtl1 <knnthcrtl.kc@gmail.com>
2026-03-07 14:34:31 +0800

    fix layout

--- apps/resident-app/app/_layout.tsx
+++ apps/resident-app/app/_layout.tsx
@@ -3,20 +3,3 @@ import { Stack } from "expo-router";
 export default function Layout() {
   return <Stack screenOptions={{ headerShown: true }} />;
 }
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
