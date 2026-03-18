acbb7a54ffad601f8dc13c8d33e4a952812edb92
knnthcrtl1 <knnthcrtl.kc@gmail.com>
2026-03-18 22:44:26 +0800

    fix  app service provider

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->app->environment('production') || filter_var(env('FORCE_HTTPS', false), FILTER_VALIDATE_BOOL)) {
+            URL::forceScheme('https');
+
+            if ($appUrl = env('APP_URL')) {
+                URL::forceRootUrl(rtrim($appUrl, '/'));
+            }
+        }
     }
 }
